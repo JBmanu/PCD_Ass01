7073e3057e1e798a4df3d072e5df66f32eab5d53
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-04-06 22:24:19 +0200

    feat: green divideEquallyListWithElementsLessThatDivisor

--- src/utils/ListUtils.java
+++ src/utils/ListUtils.java
@@ -1,15 +1,31 @@
 package utils;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ListUtils {
-    public static <T> List<List<T>> divideEqually(List<T> lista, int dimensione) {
-        return lista.parallelStream()
-                .collect(Collectors.groupingByConcurrent(i -> Math.floorDiv(lista.indexOf(i), dimensione)))
-                .values().stream()
-                .map(ArrayList::new)
+    public static <T> List<List<T>> divideEqually(List<T> lista, int divisore) {
+        int numSottoListe = Math.min(divisore, lista.size());
+        int dimensioneSottoLista = lista.size() / numSottoListe;
+        int elementiExtra = lista.size() % numSottoListe;
+
+        return IntStream.range(0, numSottoListe)
+                .mapToObj(i -> {
+                    int startIndex = i * dimensioneSottoLista + Math.min(i, elementiExtra);
+                    int endIndex = startIndex + dimensioneSottoLista + (i < elementiExtra ? 1 : 0);
+                    return lista.subList(startIndex, endIndex);
+                })
                 .collect(Collectors.toList());
+
+//        return dimensione >= lista.size() ?
+//                lista.stream()
+//                        .map(List::of)
+//                        .collect(Collectors.toList()) :
+//                lista.stream()
+//                        .collect(Collectors.groupingBy(i -> Math.floorDiv(lista.indexOf(i), dimensione)))
+//                        .values().stream()
+//                        .map(ArrayList::new)
+//                        .collect(Collectors.toList());
     }
 }
--- test/utils/ListUtilsTest.java
+++ test/utils/ListUtilsTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ListUtilsTest {
 
     @Test
@@ -11,10 +13,26 @@ public class ListUtilsTest {
         final List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
         final int divisor = 3;
         List<List<Integer>> dividedList = ListUtils.divideEqually(list, divisor);
-        assert dividedList.size() == 3;
-        assert dividedList.get(0).size() == 3;
-        assert dividedList.get(1).size() == 3;
-        assert dividedList.get(2).size() == 3;
+        assertEquals(3, dividedList.size());
+        assertEquals(3, dividedList.get(0).size());
+        assertEquals(3, dividedList.get(1).size());
+        assertEquals(3, dividedList.get(2).size());
     }
+
+    @Test
+    public void divideEquallyListWithElementsLessThatDivisor() {
+        final List<Integer> list = List.of(1, 2, 3);
+        final int divisor = 5;
+        List<List<Integer>> dividedList = ListUtils.divideEqually(list, divisor);
+        assertEquals(3, dividedList.size());
+        assertEquals(1, dividedList.get(0).size());
+        assertEquals(1, dividedList.get(1).size());
+        assertEquals(1, dividedList.get(2).size());
+    }
+
+
+
+
+
 
 }

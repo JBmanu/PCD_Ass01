913bc4d074d59eb47242bf57d496018a4e0eb467
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-04-07 17:02:49 +0200

    feat: trafficLight, it works

--- src/trafficexamples/RunTrafficSimulation.java
+++ src/trafficexamples/RunTrafficSimulation.java
@@ -12,9 +12,9 @@ public class RunTrafficSimulation {
 	public static void main(final String[] args) {
 
 //		final var simulation = new TrafficSimulationSingleRoadTwoCars();
-		 var simulation = new TrafficSimulationSingleRoadSeveralCars();
+//		 var simulation = new TrafficSimulationSingleRoadSeveralCars();
 //		 var simulation = new TrafficSimulationSingleRoadWithTrafficLightTwoCars();
-		// var simulation = new TrafficSimulationWithCrossRoads();
+		 var simulation = new TrafficSimulationWithCrossRoads();
 		simulation.setup();
 
 		final RoadSimView view = new RoadSimView();
